30bb43409d66ce82a05437e651947cb815301ed7
Jannick <jl-p@hotmail.com>
2019-11-18 21:24:12 +0100

    Test adding controller, repo and model

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import com.example.demo.repo.PersonRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    PersonRepo pRepo;
+
+}
